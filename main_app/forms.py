0c86935d84e6a84e04613cf8895d0e42cb222967
peterpickles <lipeter2006@gmail.com>
2018-02-08 16:53:52 -0800

    getting readme into the repo

---
+++ main_app/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class TreasureForm(forms.Form):
+    name = forms.CharField(label='Name', max_length=100)
+    value = forms.DecimalField(label='Value', max_digits=10, decimal_places=2)
+    material = forms.CharField(label='Material', max_length=100)
+    location = forms.CharField(label='Location', max_length=100)
--- main_app/models.py
+++ main_app/models.py
@@ -5,6 +5,7 @@ class Treasure(models.Model):
     value = models.DecimalField(max_digits=10, decimal_places=2)
     material = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
+    # user = models.ForeignKey(User)
     
     def __str__(self):
 	    return self.name
